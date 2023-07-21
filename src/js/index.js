cf7b937c9547a876246185c87e213f0f692ad406
ok-qa <olechka.kr@gmail.com>
2023-07-21 01:08:23 -0700

    markup

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,19 @@
+import SlimSelect from 'slim-select';
+new SlimSelect({
+  select: '#selectElement',
+});
+
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import '../css/index.css';
+import { createMarkup } from './markups';
+
+const loader = document.querySelector('.loader');
+const selectBreed = document.querySelector('.breed-select');
+const errorMsg = document.querySelector('.error');
+const backdrop = document.querySelector('#backdrop');
+const catModal = document.querySelector('#cat-card-content');
+const closeButton = document.querySelector('#close-button');
+
+let pickedBred = null;
+let breeds = [];
+
